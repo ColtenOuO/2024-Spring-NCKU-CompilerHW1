2d3eed7b230dfd34ffd2dfc5295947168fb862c1
WavJaby <WavJaby@gmail.com>
2024-03-15 06:13:58 +0800

    add judge score

--- judge.sh
+++ judge.sh
@@ -13,6 +13,7 @@ specific_test=""
 specific_subtest=""
 specific_testcase=""
 specific_test_find=false
+score=0
 
 usage() {
     echo "Usage: $0 [-d|--diff] [--case=subtask/testcase] [-h|--help]"
@@ -54,6 +55,26 @@ done
 
 make build
 
+get_score() {
+    local subtask="$1"
+    case "$subtask" in 
+        subtask01-helloworld) return 8 ;;
+        subtask02-comment) return 8 ;;
+        subtask03-precedenc) return 8 ;;
+        subtask04-assigment) return 8 ;;
+        subtask05-casting) return 8 ;;
+        subtask06-if) return 8 ;;
+        subtask07-while) return 8 ;;
+        subtask08-for) return 8 ;;
+        subtask09-function) return 8 ;;
+        subtask10-array) return 8 ;;
+        subtask11-autotype) return 8 ;;
+        subtask12-loop2) return 10 ;;
+        subtask13-2Darray) return 8 ;;
+        subtask14-forall) return 14 ;;
+    esac
+}
+
 run_test() {
     local subtask="$1"
     local testcase="$2"
@@ -69,6 +90,7 @@ run_test() {
             diff_output=$(git diff --no-prefix --no-index --ignore-cr-at-eol --color "$answer_dir/$subtask/$testcase.out" "$output_file")
             if [ -z "$diff_output" ]; then
                 echo -e "✅$GREEN $testcase output matches expected$RESET"
+                return 1
             else
                 echo -e "❌$RED '$subtask/$testcase' output does not match expected$RESET"
                 if [ "$show_diff" = true ]; then
@@ -95,6 +117,7 @@ for sub_dir in "$input_dir"/*; do
     fi
 
     echo "########## $subtask ##########"
+    pass_subtask=true
     # Iterate through each testcase
     for testcase_file in "$sub_dir"/*.cpp; do
         # Extract testcase filename without extension
@@ -105,7 +128,15 @@ for sub_dir in "$input_dir"/*; do
         fi
         specific_test_find=true
         run_test "$subtask" "$testcase"
+        # Check if test pass
+        if [ $? != 1 ]; then
+            pass_subtask=false
+        fi
     done
+    if [ $pass_subtask = true ]; then
+        get_score "$subtask"
+        score=$((score+$?))
+    fi
 done
 
 if [ -n "$specific_test" ] && [ $specific_test_find = false ]; then
@@ -118,4 +149,6 @@ if [ -n "$specific_test" ] && [ $specific_test_find = false ]; then
             echo "$subtask/$testcase"
         done
     done
+else
+    echo "Score: $score"
 fi
